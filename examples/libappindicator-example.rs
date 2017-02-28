251fb7ea6f2be2169028e1ce7164be2725a9b7cc
Kyle Machulis <kyle@nonpolynomial.com>
2017-02-27 23:57:19 -0800

    chore: Simplify str to gchar* conversions

--- examples/libappindicator-example.rs
+++ examples/libappindicator-example.rs
@@ -7,9 +7,9 @@ use libappindicator::{AppIndicator, AppIndicatorStatus};
 
 fn main() {
     gtk::init().unwrap();
-    let mut indicator = AppIndicator::new("libappindicator test application", "").unwrap();
+    let mut indicator = AppIndicator::new("libappindicator test application", "");
     indicator.set_status(AppIndicatorStatus::APP_INDICATOR_STATUS_ACTIVE);
-    indicator.set_icon_full("/usr/share/gxkb/flags/ua.png", "icon").unwrap();
+    indicator.set_icon_full("/usr/share/gxkb/flags/ua.png", "icon");
     let mut m = gtk::Menu::new();
     let mi = gtk::ImageMenuItem::new_from_stock(gtk_sys::GTK_STOCK_QUIT, None);
     mi.connect_activate(|_| {
--- src/lib.rs
+++ src/lib.rs
@@ -6,31 +6,20 @@ extern crate gtk_sys;
 pub use libappindicator_sys::*;
 use libappindicator_sys::{AppIndicator as AppIndicatorRaw};
 use glib::translate::{ToGlibPtr};
-use std::ffi::{CString, NulError};
 
 pub struct AppIndicator {
     air: *mut AppIndicatorRaw
 }
 
 impl AppIndicator {
-    pub fn new(title: &str, icon: &str) -> Result<AppIndicator, NulError> {
-        let ai;
-        let title = match CString::new(title) {
-            Ok(t) => t,
-            Err(e) => return Err(e)
-        };
-        let icon = match CString::new(icon) {
-            Ok(i) => i,
-            Err(e) => return Err(e)
-        };
-        unsafe {
-            ai = app_indicator_new(title.as_ptr(),
-                                   icon.as_ptr(),
-                                   AppIndicatorCategory::APP_INDICATOR_CATEGORY_APPLICATION_STATUS);
+    pub fn new(title: &str, icon: &str) -> AppIndicator {
+        AppIndicator {
+            air: unsafe {
+                app_indicator_new(title.to_glib_none().0,
+                                  icon.to_glib_none().0,
+                                  AppIndicatorCategory::APP_INDICATOR_CATEGORY_APPLICATION_STATUS)
+            }
         }
-        Ok(AppIndicator {
-            air: ai
-        })
     }
 
     pub fn set_status(&mut self, status: AppIndicatorStatus) {
@@ -40,55 +29,27 @@ impl AppIndicator {
     }
 
     pub fn set_menu(&mut self, menu: &mut gtk::Menu) {
-        let mp = menu.to_glib_none();
         unsafe {
-            app_indicator_set_menu(self.air, mp.0);
+            app_indicator_set_menu(self.air, menu.to_glib_none().0);
         }
     }
 
-    pub fn set_label(&mut self, label: &str, guide: &str) -> Result<(), NulError> {
-        let label = match CString::new(label) {
-            Ok(t) => t,
-            Err(e) => return Err(e)
-        };
-        let guide = match CString::new(guide) {
-            Ok(i) => i,
-            Err(e) => return Err(e)
-        };
+    pub fn set_label(&mut self, label: &str, guide: &str) {
         unsafe {
-            app_indicator_set_label(self.air, label.as_ptr(), guide.as_ptr());
+            app_indicator_set_label(self.air, label.to_glib_none().0, guide.to_glib_none().0);
         }
-        Ok(())
     }
 
-    pub fn set_icon_full(&mut self, name: &str, desc: &str) -> Result<(), NulError> {
-        let name = match CString::new(name) {
-            Ok(t) => t,
-            Err(e) => return Err(e)
-        };
-        let desc = match CString::new(desc) {
-            Ok(i) => i,
-            Err(e) => return Err(e)
-        };
+    pub fn set_icon_full(&mut self, name: &str, desc: &str) {
         unsafe {
-            app_indicator_set_icon_full(self.air, name.as_ptr(), desc.as_ptr());
+            app_indicator_set_icon_full(self.air, name.to_glib_none().0, desc.to_glib_none().0);
         }
-        Ok(())
     }
 
-    pub fn set_attention_icon_full(&mut self, name: &str, desc: &str) -> Result<(), NulError>  {
-        let name = match CString::new(name) {
-            Ok(t) => t,
-            Err(e) => return Err(e)
-        };
-        let desc = match CString::new(desc) {
-            Ok(i) => i,
-            Err(e) => return Err(e)
-        };
+    pub fn set_attention_icon_full(&mut self, name: &str, desc: &str) {
         unsafe {
-            app_indicator_set_attention_icon_full(self.air, name.as_ptr(), desc.as_ptr());
+            app_indicator_set_attention_icon_full(self.air, name.to_glib_none().0, desc.to_glib_none().0);
         }
-        Ok(())
     }
 }
 
