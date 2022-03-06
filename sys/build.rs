37b360de8ba9d3cc84594fa3f3b5f63e38a1e7ad
Edwin Svensson <git@olback.net>
2022-03-06 23:27:52 +0100

    support ayatana-appindicator3

--- sys/build.rs
+++ sys/build.rs
@@ -1,9 +1,13 @@
 fn main() {
-  println!("cargo:rustc-link-lib=appindicator3");
-
-  if let Err(_) = pkg_config::probe_library("appindicator3") {
-    if let Err(_) = pkg_config::probe_library("appindicator3-0.1") {
-      panic!("libappindicator3 library not found!");
-    }
-  }
+  if pkg_config::probe_library("appindicator3").is_ok()
+    || pkg_config::probe_library("appindicator3-0.1").is_ok()
+  {
+    println!("cargo:rustc-link-lib=appindicator3");
+  } else if pkg_config::probe_library("ayatana-appindicator3").is_ok()
+    || pkg_config::probe_library("ayatana-appindicator3-0.1").is_ok()
+  {
+    println!("cargo:rustc-link-lib=ayatana-appindicator3");
+  } else {
+    panic!("libappindicator3 or libayatana-appindicator3 library not found!");
+  };
 }
