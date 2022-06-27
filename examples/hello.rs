e73f4534f30f666cbbd377186537cbcfda6a2a2c
Beanow <497556+Beanow@users.noreply.github.com>
2022-06-27 20:43:32 +0200

    Add ENV arguments to example

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,5 +1,7 @@
-use std::env;
-use std::path::Path;
+use std::{
+  env,
+  path::{Path, PathBuf},
+};
 
 use gtk::prelude::*;
 use libappindicator::{AppIndicator, AppIndicatorStatus};
@@ -8,9 +10,19 @@ fn main() {
   gtk::init().unwrap();
   let mut indicator = AppIndicator::new("libappindicator test application", "");
   indicator.set_status(AppIndicatorStatus::Active);
-  let icon_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("examples");
+
+  let icon_path = match env::var("TRAY_ICON_DIR") {
+    Ok(dir) => PathBuf::from(dir),
+    _ => Path::new(env!("CARGO_MANIFEST_DIR")).join("examples"),
+  };
+
+  let icon_name = match env::var("TRAY_ICON_NAME") {
+    Ok(name) => name,
+    _ => "rust-logo".to_string(),
+  };
+
   indicator.set_icon_theme_path(icon_path.to_str().unwrap());
-  indicator.set_icon_full("rust-logo", "icon");
+  indicator.set_icon_full(&icon_name, "icon");
   let mut m = gtk::Menu::new();
   let mi = gtk::CheckMenuItem::with_label("Hello Rust!");
   mi.connect_activate(|_| {
