b38c209605c1f89c3762e538615f3ec70b73193b
Beanow <497556+Beanow@users.noreply.github.com>
2022-06-27 20:06:22 +0200

    fix(sys): load exclusively using dynamic linking
    This should let `dlopen` (through `ld.so`) handle what paths to search in for the respective libraries.
    
    Additionally this fixes a mistake with the library filenames. Now using the `SONAME` instead of a symlinked name that happened to work when dev packages are installed.

--- sys/src/lib.rs
+++ sys/src/lib.rs
@@ -8,73 +8,25 @@ use gtk_sys::{
 };
 use libloading::*;
 use once_cell::sync::Lazy;
-use std::{os::raw::*, path::PathBuf, process::Command};
+use std::os::raw::*;
 
 pub static LIB: Lazy<Library> = Lazy::new(|| {
-  #[cfg(target_os = "linux")]
-  {
-    if let Some(appimage_path) = std::env::var_os("APPDIR") {
-      // validate that we're actually running on an AppImage
-      // an AppImage is mounted to `/$TEMPDIR/.mount_${appPrefix}${hash}`
-      // see https://github.com/AppImage/AppImageKit/blob/1681fd84dbe09c7d9b22e13cdb16ea601aa0ec47/src/runtime.c#L501
-      // note that it is safe to use `std::env::current_exe` here since we just loaded an AppImage.
-      let is_temp = std::env::current_exe()
-        .map(|p| {
-          p.display()
-            .to_string()
-            .starts_with(&format!("{}/.mount_", std::env::temp_dir().display()))
-        })
-        .unwrap_or(true);
-
-      if !is_temp {
-        panic!("`APPDIR` environment variable found but this application was not detected as an AppImage; this might be a security issue.");
-      }
-
-      let appimage_path = PathBuf::from(appimage_path);
-      let ayatana_target_path = appimage_path.join("usr/lib/libayatana-appindicator3.so");
-      let gtk_target_path = appimage_path.join("usr/lib/libappindicator3.so");
-
-      if ayatana_target_path.exists() {
-        return unsafe { Library::new(ayatana_target_path).unwrap() };
-      } else if gtk_target_path.exists() {
-        return unsafe { Library::new(gtk_target_path).unwrap() };
-      }
-    }
+  let libayatana = unsafe { Library::new("libayatana-appindicator3.so.1") };
+  if let Ok(lib) = libayatana {
+    return lib;
   }
-  // PKG_CONFIG_ALLOW_SYSTEM_LIBS=1 pkg-config --libs-only-L ayatana-appindicator3-0.1
-  let path = get_appindicator_library_path();
-  unsafe { Library::new(path).unwrap() }
+
+  let libappindicator = unsafe { Library::new("libappindicator3.so.1") };
+  if let Ok(lib) = libappindicator {
+    return lib;
+  }
+
+  panic!(
+    "Failed to load libayatana-appindicator3.so.1 or libappindicator3.so.1\n{}\n{}",
+    libayatana.unwrap_err(),
+    libappindicator.unwrap_err()
+  );
 });
-
-/// Gets the path to the target appindicator library file (`.so` extension).
-pub fn get_appindicator_library_path() -> PathBuf {
-  match get_library_path("ayatana-appindicator3-0.1") {
-    Some(p) => format!("{}/libayatana-appindicator3.so", p).into(),
-    None => match get_library_path("appindicator3-0.1") {
-      Some(p) => format!("{}/libappindicator3.so", p).into(),
-      None => panic!("Can't detect any appindicator library"),
-    },
-  }
-}
-
-/// Gets the folder in which a library is located using `pkg-config`.
-pub fn get_library_path(name: &str) -> Option<String> {
-  let mut cmd = Command::new("pkg-config");
-  cmd.env("PKG_CONFIG_ALLOW_SYSTEM_LIBS", "1");
-  cmd.arg("--libs-only-L");
-  cmd.arg(name);
-  if let Ok(output) = cmd.output() {
-    if !output.stdout.is_empty() {
-      // output would be "-L/path/to/library\n"
-      let word = output.stdout[2..].to_vec();
-      return Some(String::from_utf8_lossy(&word).trim().to_string());
-    } else {
-      return None;
-    }
-  } else {
-    return None;
-  }
-}
 
 pub type guint32 = c_uint;
 pub type gint64 = c_long;
